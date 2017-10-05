9c24e8d294615d8f55e91b76b255418b4446d81e
FuryFire <furyfire@users.noreply.github.com>
2017-10-05 16:57:53 +0200

    Cleanup. Added usercount. Added connection status. State is now sent to robot interface also

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { origins: '*:*'});
+var listening_port = process.env.PORT || 80;
 
-//var mqtt = require('mqtt')
-//var client  = mqtt.connect('mqtt://test.mosquitto.org')
-
-//client.on('connect', function () {
-//	console.log("Connected to MQTT");
-//})
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
-
 app.get('/robot', function(req, res){
   res.sendFile(__dirname + '/robot.html');
 });
+
 io.on('connection', function (socket) {
-
-
-    console.log('a user connected');
-
+    console.log(socket.id + ": Connected");
+    
+    socket.emit("user_count", io.engine.clientsCount);
+    
+    socket.on('disconnect', function () {
+        console.log(socket.id + ": Disconnected");
+        socket.broadcast.emit("user_count", io.engine.clientsCount);
+    });
+    
     socket.on('start', function () {
-        console.log("Moving");
+        console.log(socket.id + ": Moving");
+         socket.broadcast.emit("move");
     });
     socket.on('end', function () {
-        console.log("Stopped");
+        console.log(socket.id + ": Stopped");
+        socket.broadcast.emit("stop");
     });
     socket.on('move', function (force, angle) {
-//        console.log(new Date().getTime()/1000 + '\t'+force + "\t"+ angle);
           socket.broadcast.emit('data', force, angle);
-//	client.publish("homie/18fe34d4e680/robot/move/set", force +","+angle);
     });
 });
 
-http.listen(process.env.PORT || 80, function(){
-  console.log('listening on *');
+http.listen(listening_port, '0.0.0.0', function(){
+  console.log('listening on', listening_port);
 });
