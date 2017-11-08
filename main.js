36eb43dfddfa089a03c8412fc0c35b94e1a33546
FuryFire <furyfire@users.noreply.github.com>
2017-11-08 13:40:16 +0100

    Fjernet usercount fra interfacet. Strømlinet websockets event navne så de matcher dokumentationen. Implementeret 15 sekunders timeout hvorefter der disconnectes. Implementeret rød/grøn background

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { origins: '*:*'});
 
-// Listening port er port 80 fra web servers perspektiv
+//Listening port er port 80 fra web servers perspektiv
+//Accepter også port fra enviroment variable
 var listening_port = process.env.PORT || 80; 
 
 // Her henter vi 
@@ -15,40 +16,44 @@ app.get('/robot', function(req, res){
   res.sendFile(__dirname + '/robot.html');
 });
 
-/* Hvis der åbnes en connection.
-   I denne implementation af koden vil vi modtage data fra klienten og printe det ud
-   med console.log() funktionen, i den endelige implementation vil vi behandle dataen
-   og omsætte det til hvor hurtigt de to larvebånd skal dreje rundt, og i hvilken retning */
+/*
+Event når der åbnes en connection.
+I denne implementation af koden vil vi modtage data fra klienten og printe det ud
+med console.log() funktionen og sende det til alle connectede klienter 
+*/
 io.on('connection', function (socket) {
+    //Debug
     console.log(socket.id + ": Connected");
 	
     // io.engine.clientsCount indeholder antallet af aktive connections
-    socket.emit("user_count", io.engine.clientsCount); 
+    //Send til alle
+    socket.broadcast.emit("user_count", io.engine.clientsCount); 
     
-	// Hvis der disconnectes. skriv hvem der disconnectede og hvor mange der nu er tilbage
+    //Hvis der disconnectes. skriv hvem der disconnectede 
     socket.on('disconnect', function () {
         console.log(socket.id + ": Disconnected");
         socket.broadcast.emit("user_count", io.engine.clientsCount);
     });
     
-	// Hvis joysticket betjenes, skriv hvem der betjener det
+    // Hvis joysticket betjenes
     socket.on('start', function () {
-        console.log(socket.id + ": Moving");
-         socket.broadcast.emit("move");
+        console.log(socket.id + ": Start");
+        socket.broadcast.emit("start");
     });
 	
-	// Hvis joysticket slippes, skriv hvem der slap det
+    // Hvis joysticket slippes,
     socket.on('end', function () {
-        console.log(socket.id + ": Stopped");
-        socket.broadcast.emit("stop");
+        console.log(socket.id + ": End");
+        socket.broadcast.emit("end");
     });
 	
-	// Hvis joystickets position ændres, skriv hvortil
+    // Hvis joystickets position ændres, skriv hvorti
     socket.on('move', function (force, angle) {
-          socket.broadcast.emit('data', force, angle);
+        console.log(socket.id + ": Move",force,angle)
+        socket.broadcast.emit('move', force, angle);
     });
 });
 
 http.listen(listening_port, '0.0.0.0', function(){
-  console.log('listening on', listening_port);
+    console.log('listening on', listening_port);
 });
